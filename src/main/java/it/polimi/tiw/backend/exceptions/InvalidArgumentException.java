157442bfc2928bc90372d5d269be86909aed2715
lorenzo-cmyk <10797603@polimi.it>
2024-07-14 17:36:30 +0200

    [backend.beans] Added InvalidArgumentException.java and User.java

---
+++ src/main/java/it/polimi/tiw/backend/exceptions/InvalidArgumentException.java
@@ -0,0 +1,15 @@
+package it.polimi.tiw.backend.exceptions;
+
+/**
+ * This exception is thrown when an invalid argument is provided to a method.
+ */
+public class InvalidArgumentException extends Exception {
+    /**
+     * This constructor is used to create a new exception with a message.
+     *
+     * @param message the message of the exception
+     */
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+}
