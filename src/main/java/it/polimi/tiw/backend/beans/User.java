cdfa91fd6ed3a584009a5440ca887d8ad49bdfbb
lorenzo-cmyk <10797603@polimi.it>
2024-07-14 18:34:57 +0200

    [User.java] Removed useless userID attribute

--- src/main/java/it/polimi/tiw/backend/beans/User.java
+++ src/main/java/it/polimi/tiw/backend/beans/User.java
@@ -10,7 +10,6 @@ import static it.polimi.tiw.backend.utilities.Validators.isStringValid;
  * This class cannot be a record because it needs to throw an exception in the constructor.
  */
 public class User {
-    private final int userID;
     private final String username;
     private final String passwordHash;
     private final String eMail;
@@ -18,14 +17,13 @@ public class User {
     /**
      * This constructor is used to create a new user. It checks that all the fields are not null.
      *
-     * @param userID       the ID of the user
      * @param username     the username of the user
      * @param passwordHash the password hash of the user
      * @param eMail        the email of the user
      * @throws InvalidArgumentException if any of the fields is invalid
      */
-    public User(int userID, String username, String passwordHash, String eMail) throws InvalidArgumentException {
-        if (userID < 0 || !isStringValid(username) || !isStringValid(passwordHash) || !isStringValid(eMail)) {
+    public User(String username, String passwordHash, String eMail) throws InvalidArgumentException {
+        if (!isStringValid(username) || !isStringValid(passwordHash) || !isStringValid(eMail)) {
             throw new InvalidArgumentException("The arguments provided are not valid.");
         } else if (username.length() > 64 || passwordHash.length() > 128 || eMail.length() > 64) {
             throw new InvalidArgumentException("The arguments provided exceed the maximum length.");
@@ -33,19 +31,9 @@ public class User {
             throw new InvalidArgumentException("The email provided is not syntactically valid.");
         }
 
-        this.userID = userID;
         this.username = username;
         this.passwordHash = passwordHash;
         this.eMail = eMail;
-    }
-
-    /**
-     * This method returns the ID of the user.
-     *
-     * @return the ID of the user
-     */
-    public int getUserID() {
-        return userID;
     }
 
     /**
