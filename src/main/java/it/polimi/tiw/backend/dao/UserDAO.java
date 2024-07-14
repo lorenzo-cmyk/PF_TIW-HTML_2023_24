500c59b78a9dc04e91835bd4abd9f51ecede7604
lorenzo-cmyk <10797603@polimi.it>
2024-07-14 20:37:53 +0200

    [UserDAO.java] Made the registration of a user an atomic operation

--- src/main/java/it/polimi/tiw/backend/dao/UserDAO.java
+++ src/main/java/it/polimi/tiw/backend/dao/UserDAO.java
@@ -35,20 +35,41 @@ public class UserDAO {
      * @throws RegistrationException if the registration fails.
      */
     public void registerUser(User newUser) throws SQLException, RegistrationException {
-        // The raw SQL query for registering a new user.
-        String registrationQuery = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (?, ?, ?)";
+        // Since we are writing to the database, we need to use a transaction
+        // in order to ensure that the operation is atomic.
+        connection.setAutoCommit(false);
 
-        // Try-with-resources statement used to automatically close the PreparedStatement when it is no longer needed.
-        try (PreparedStatement preparedStatement = connection.prepareStatement(registrationQuery)) {
-            preparedStatement.setString(1, newUser.getUsername());
-            preparedStatement.setString(2, newUser.getPasswordHash());
-            preparedStatement.setString(3, newUser.getEmail());
+        // Try to register the new user.
+        try {
+            // The raw SQL query for registering a new user.
+            String registrationQuery = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (?, ?, ?)";
 
-            if (preparedStatement.executeUpdate() == 0) {
-                throw new RegistrationException("Unable to register user. " +
-                        "Check if the interested username is already taken.");
+            // Try-with-resources statement used to automatically close the PreparedStatement when it is no longer needed.
+            try (PreparedStatement preparedStatement = connection.prepareStatement(registrationQuery)) {
+                // Set the parameters of the query.
+                preparedStatement.setString(1, newUser.getUsername());
+                preparedStatement.setString(2, newUser.getPasswordHash());
+                preparedStatement.setString(3, newUser.getEmail());
 
+                // Execute the query and check if it had any effect.
+                if (preparedStatement.executeUpdate() == 0) {
+                    throw new RegistrationException("Unable to register user. " +
+                            "Check if the interested username is already taken.");
+                    // If the execution of the query returns 0, it means that no rows were affected.
+                    // This means that the user was not registered. The method throws a RegistrationException.
+                    // Since the operation had no effect we don't need to commit the transaction.
+                }
+
+                // The user was successfully registered, so we can safely commit the transaction.
+                connection.commit();
             }
+        } catch (SQLException e) {
+            // If an error occurs during the registration process, we need to roll back the transaction.
+            connection.rollback();
+            throw e; // Re-throw the exception to the caller.
+        } finally {
+            // Restore the default behavior of the connection.
+            connection.setAutoCommit(true);
         }
     }
 }
