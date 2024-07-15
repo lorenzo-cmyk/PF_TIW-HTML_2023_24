43f0bbf93804d082b560436d4ddb5923aeaa6fff
lorenzo-cmyk <10797603@polimi.it>
2024-07-15 23:21:31 +0200

    [Validators.java] Added isIDValid() and isDateValid() methods

--- src/main/java/it/polimi/tiw/backend/utilities/Validators.java
+++ src/main/java/it/polimi/tiw/backend/utilities/Validators.java
@@ -2,10 +2,32 @@ package it.polimi.tiw.backend.utilities;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.util.Date;
+
 /**
  * This class contains utility methods to validate data.
  */
 public class Validators {
+    /**
+     * This method checks if a integer ID is valid. An integer ID is valid if it is greater than 0.
+     *
+     * @param id the integer ID to check
+     * @return true if the integer ID is valid, false otherwise
+     */
+    public static boolean isIDValid(int id) {
+        return id > 0;
+    }
+
+    /**
+     * This method checks if a Date is valid. A Date is valid if it is not null.
+     *
+     * @param date the Date to check
+     * @return true if the Date is valid, false otherwise
+     */
+    public static boolean isDateValid(Date date) {
+        return date != null;
+    }
+
     /**
      * This method checks if a string is valid. A string is valid if it is not null, not empty and not blank.
      *
