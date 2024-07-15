0b577b1770a09d8179ff206103e99876d601ee98
lorenzo-cmyk <10797603@polimi.it>
2024-07-15 14:12:06 +0200

    [UserDAO.java] Changed RegistrationException message

--- src/main/java/it/polimi/tiw/backend/dao/UserDAO.java
+++ src/main/java/it/polimi/tiw/backend/dao/UserDAO.java
@@ -61,8 +61,8 @@ public class UserDAO {
             if (e.getErrorCode() == 1062) {
                 // If the error is due to a unique constraint violation, we throw a RegistrationException.
                 // This errorCode is specific to MySQL, and it means that a unique constraint was violated.
-                throw new RegistrationException("Unable to register user. " +
-                        "Check if the interested username is already taken.");
+                throw new RegistrationException("Unable to register user." +
+                        " The username maybe already taken. Please try again with a different one.");
             } else {
                 // If the error is due to another reason, we re-throw the exception.
                 throw e;
